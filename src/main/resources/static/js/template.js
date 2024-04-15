b5cda0242dd14e53e1946f8cc5f51f7e9e039480
Givebuff <ckdwls0275@gmail.com>
2024-04-15 17:04:50 +0900

    menu order page add, template js code refactoring

--- src/main/resources/static/js/template.js
+++ src/main/resources/static/js/template.js
@@ -1,3 +1,44 @@
+//홀 디자인
+var holeTemplate = {
+    tableButtonStylePrefix : function(tableNum){
+        // 홀 테이블 버튼 디자인 시작
+        return "<button value='" + tableNum + "' class='"
+                    + "inline-flex items-center justify-center whitespace-nowrap rounded-md "
+                    + "text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none "
+                    + "focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 "
+                    + "disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground "
+                    + "hover:bg-primary/90 h-10 px-4 py-2 ";
+    },
+    tableButtonStyleSuffix : function(){
+        // 홀 테이블 버튼 디자인 종료
+        return "</button>";
+    },
+    unusedTableButtonStyle : function(tableNum){
+        // 선택되지 않은 홀 테이블 버튼
+        return holeTemplate.tableButtonStylePrefix(tableNum)
+                + "hover:bg-gray-400 '>"
+                + tableNum
+                + holeTemplate.tableButtonStyleSuffix();
+    },
+    usedTableButtonStyle : function(tableNum){
+        // 선택된 홀 테이블 버튼
+        return holeTemplate.tableButtonStylePrefix(tableNum)
+               + "bg-green-100 hover:bg-gray-400'>"
+               + tableNum
+               + holeTemplate.tableButtonStyleSuffix();
+    }
+};
+
+var template = {
+    menuOrderListButton : function(name) {
+        return '<label class="flex items-center space-x-2">'
+                + '<input type="radio" class="form-radio"'
+                + 'name="' + name + '">'
+                + '<span>라디오1</span>'
+                + '</label>';
+    }
+}
+
 function holeUnusedTableButtonStyle(tableNum) {
     return holeTableButtonStylePrefix(tableNum)
             + "hover:bg-gray-400 '>"
